fe913799abe579e0861c1f9430fb2e813bb120af
calebfergie <yonohabloespanol@gmail.com>
2018-04-20 19:08:20 -0400

    full trackfinding triage complete

--- lyrics/mashup-final-v1/public/javascripts/lyrics.js
+++ lyrics/mashup-final-v1/public/javascripts/lyrics.js
@@ -28,7 +28,7 @@ function searchArtist(artistQuery) {
   }).done(function(data) {
     let artistID = data.message.body.artist_list[0].artist.artist_id;
     let artistName = data.message.body.artist_list[0].artist.artist_name;
-    console.log(artistName)
+    console.log("search for " + artistQuery + " returned " + artistName);
     artistLexes.push(artistName, []);
     getAlbums(artistID, artistLexes.length);
   });
@@ -54,14 +54,10 @@ function getAlbums(aritstID) {
     }
   }).done(function(data) {
     let albumList = data.message.body.album_list;
-    console.log(albumList)
-    let albumID;
     for (i in albumList) {
-      albumID = albumList[i].album.album_id;
+      let albumID = albumList[i].album.album_id;
       getSongs(albumID);
     }
-
-
   });
 };
 
@@ -85,61 +81,34 @@ function getSongs(albumID) {
       console.log(errorThrown);
     }
   }).done(function(data) {
-    console.log(data.message.body)
-    let songlist = data.message.body;
-    tempdata = songlist;
+    let trackList = data.message.body.track_list;
+    for (i in trackList) {
+      let trackID = trackList[0].track.track_id
+      getLyrics(trackID)
+    }
   });
 };
 
-// function getAlbums(artistID, lexPosition) {
-//   $.ajax({
-//     type: "GET",
-//     data: {
-//       apikey: "0980b0d4c04de99715efa813020dc6db",
-//       artist_id: artistID,
-//       format: "jsonp",
-//       callback: "jsonp_callback"
-//     },
-//     url: "https://api.musixmatch.com/ws/1.1/artist.albums.get",
-//     dataType: "jsonp",
-//     jsonpCallback: 'jsonp_callback',
-//     contentType: 'application/json',
-//     success: function(data) {
-//       let albumList = data.message.body.album_list;
-//       for (i in albumList) {
-//         let albumID = data.message.body.album_list[i].album.album_id;
-//         getSongs(albumID, lexPosition);
-//       };
-//     },
-//     error: function(jqXHR, textStatus, errorThrown) {
-//       console.log(jqXHR);
-//       console.log(textStatus);
-//       console.log(errorThrown);
-//     }
-//   });
-// };
-//
-// function getSongs(albumID, lexPosition) {
-//   console.log("Looking up album no.  " + albumID);
-//   $.ajax({
-//     type: "GET",
-//     data: {
-//       apikey: "0980b0d4c04de99715efa813020dc6db",
-//       album_id: albumID,
-//       format: "jsonp",
-//       callback: "jsonp_callback"
-//     },
-//     url: "https://api.musixmatch.com/ws/1.1/album.tracks.get",
-//     dataType: "jsonp",
-//     jsonpCallback: 'jsonp_callback',
-//     contentType: 'application/json',
-//     success: function(data) {
-//       tempdata = data;
-//     },
-//     error: function(jqXHR, textStatus, errorThrown) {
-//       console.log(jqXHR);
-//       console.log(textStatus);
-//       console.log(errorThrown);
-//     }
-//   });
-// };
+function getLyrics(trackID) {
+  // http://api.musixmatch.com/ws/1.1/album.tracks.get?apikey=0980b0d4c04de99715efa813020dc6db&album_id=28578970
+  $.ajax({
+    type: "GET",
+    data: {
+      apikey: "0980b0d4c04de99715efa813020dc6db",
+      track_id: trackID,
+      format: "jsonp",
+      // callback: "jsonp_callback"
+    },
+    url: "https://api.musixmatch.com/ws/1.1/track.lyrics.get",
+    dataType: "jsonp",
+    // jsonpCallback: 'jsonp_callback',
+    contentType: 'application/json',
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(jqXHR);
+      console.log(textStatus);
+      console.log(errorThrown);
+    }
+  }).done(function(data) {
+    let lyrics = data.message.body.lyrics.lyrics_body;
+  });
+};
